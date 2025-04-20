e7eefc93c235f67cf4825e3ff163d5eee115d3fd
Mark Peters <mapeters101@earthlink.net>
2025-04-20 13:59:25 -0400

    Work on lesson102.

---
+++ lesson102.py
@@ -0,0 +1,69 @@
+class Linked_List:
+    def __init__(self, value):
+        self.head = {
+            'value': value,
+            'next': None
+        }
+        self.tail = self.head
+        self.length = 1
+
+    def append(self, value):
+        newNode = {
+            'value': value,
+            'next': None
+        }
+        self.tail['next'] = newNode
+        self.tail = newNode
+        self.length += 1
+        return self
+
+    def prepend(self, value):
+        newNode = {
+            'value': value,
+            'next': None
+        }
+        newNode['next'] = self.head
+        self.head = newNode
+        self.length += 1
+        return self
+
+
+my_linked_list = Linked_List(10)
+print(my_linked_list.head['value'])
+print(my_linked_list.head['next'])
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+print()
+
+result = my_linked_list.append(5)
+print(result)
+print(my_linked_list.head['value'])
+print(my_linked_list.head['next'])
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+print()
+
+result = my_linked_list.append(16)
+print(result)
+print(my_linked_list.head['value'])
+print(my_linked_list.head['next'])
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+print()
+
+result = my_linked_list.prepend(1)
+print(result)
+print(my_linked_list.head['value'])
+print(my_linked_list.head['next'])
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+print()
+
+result = my_linked_list.prepend(2)
+print(result)
+print(my_linked_list.head['value'])
+print(my_linked_list.head['next'])
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+print()
+
